62959816cbe20fe3638b303fbe1aca43a2336e80
MarutFlorin <florin.marut99@e-uvt.ro>
2024-03-12 21:28:57 +0200

    feat: Implement user form

--- sellsavvy/src/app/forms/register/register.component.ts
+++ sellsavvy/src/app/forms/register/register.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent implements OnInit {
+export class RegisterForm implements OnInit {
   registerForm!: FormGroup;
 
   constructor(private readonly _formBuilder: FormBuilder) {}
-  
+
   ngOnInit(): void {
-      this.registerForm = this._formBuilder.group({
-
-      });
+    this.registerForm = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      userName: ['', [Validators.required, Validators.maxLength(50)]],
+      phoneNumber: ['', [Validators.required]],
+      description: [''],
+      email: ['', [Validators.required, Validators.email]],
+      confirmEmail: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    });
   }
 
-  onSubmit(): void {
-
-  }
+  onSubmit(): void {}
 }
