9ea873bf32ca5ebaa6216c1ced6d383dd79798b8
MarutFlorin <florin.marut99@e-uvt.ro>
2024-04-11 15:27:16 +0300

    Use router-outlet and redirect unauthorized users

--- sellsavvy/src/app/guards/authorization.guard.ts
+++ sellsavvy/src/app/guards/authorization.guard.ts
@@ -1,8 +1,11 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { inject } from '@angular/core';
 
 export const authorizationGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
+  const router = inject(Router);
+
+  if (!authService.isTokenAvailable()) router.navigate(['**']);
   return authService.isTokenAvailable();
 };
