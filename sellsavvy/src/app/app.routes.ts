d28e4baad2ce6dbb89a85b164db6fcb4318223f3
MarutFlorin <florin.marut99@e-uvt.ro>
2024-04-11 14:52:46 +0300

    Create basic pages

--- sellsavvy/src/app/app.routes.ts
+++ sellsavvy/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { UnauthorizedPageComponent } from './pages/unauthorized-page/unauthorized-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'articles',
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'articles',
+    component: ArticlesPageComponent,
+  },
+  {
+    path: '**',
+    component: UnauthorizedPageComponent,
+    pathMatch: 'full',
+  },
+];
