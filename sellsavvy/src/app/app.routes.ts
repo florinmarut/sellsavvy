8d52a784d4010cdc4f796f603dd681b2bdb724c6
MarutFlorin <florin.marut99@e-uvt.ro>
2024-04-11 15:24:18 +0300

    Create profile page and protect it with guard

--- sellsavvy/src/app/app.routes.ts
+++ sellsavvy/src/app/app.routes.ts
@@ -3,12 +3,19 @@ import { UnauthorizedPageComponent } from './pages/unauthorized-page/unauthorize
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { authorizationGuard } from './guards/authorization.guard';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'articles',
     pathMatch: 'full',
+  },
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
+    canActivate: [authorizationGuard],
   },
   {
     path: 'register',
