4ecfdb2ed8089f83f36cbab9e7950196caa6c91c
MarutFlorin <florin.marut99@e-uvt.ro>
2024-05-07 16:48:17 +0300

    Get user data to display

--- sellsavvy/src/app/pages/profile-page/profile-page.component.ts
+++ sellsavvy/src/app/pages/profile-page/profile-page.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { MatButtonModule } from '@angular/material/button';
-import { SingleFileUploadComponent } from "../../components/single-file-upload/single-file-upload.component";
+import { SingleFileUploadComponent } from '../../components/single-file-upload/single-file-upload.component';
+import { Subject, takeUntil } from 'rxjs';
+import { UserDTO } from '../../models/dtos/user.model';
 
 @Component({
-    selector: 'app-profile-page',
-    standalone: true,
-    templateUrl: './profile-page.component.html',
-    styleUrl: './profile-page.component.scss',
-    imports: [MatButtonModule, SingleFileUploadComponent]
+  selector: 'app-profile-page',
+  standalone: true,
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss',
+  imports: [MatButtonModule, SingleFileUploadComponent],
 })
-export class ProfilePageComponent implements OnInit {
+export class ProfilePageComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject();
+  
+  user: UserDTO | undefined;
+
   constructor(private readonly authService: AuthenticationService) {}
 
   ngOnInit() {
-    this.authService.getProfile().subscribe((value) => {
-      console.log('User is: ' + value.email);
-    });
+    this.authService
+      .getProfile()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (user: UserDTO) => {
+          console.log(`The user is ${user.email}`);
+          this.user = user;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
