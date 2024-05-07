f6e403164e61fe77ec16cc66658d1394e47f0231
MarutFlorin <florin.marut99@e-uvt.ro>
2024-05-07 16:31:17 +0300

    Add profile page

--- sellsavvy/src/app/pages/profile-page/profile-page.component.ts
+++ sellsavvy/src/app/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
 })
