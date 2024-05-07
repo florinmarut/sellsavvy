c7de14209520cf1ef88aaedcdf7386f5a687ef62
MarutFlorin <florin.marut99@e-uvt.ro>
2024-05-07 16:12:36 +0300

    Create single file upload component

--- sellsavvy/src/app/components/nav/navigation/navigation.component.ts
+++ sellsavvy/src/app/components/nav/navigation/navigation.component.ts
@@ -6,21 +6,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CustomSidenavComponent } from '../custom-sidenav/custom-sidenav.component';
+import { SingleFileUploadComponent } from "../../single-file-upload/single-file-upload.component";
 
 @Component({
-  selector: 'navigation',
-  standalone: true,
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss',
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    CustomSidenavComponent,
-  ],
+    selector: 'navigation',
+    standalone: true,
+    templateUrl: './navigation.component.html',
+    styleUrl: './navigation.component.scss',
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        CustomSidenavComponent,
+        SingleFileUploadComponent
+    ]
 })
 export class NavigationComponent {
   collapsed = signal(true);
