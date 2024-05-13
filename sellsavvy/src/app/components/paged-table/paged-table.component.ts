040c02c918f8f1c490d64698a822ad73714071f0
MarutFlorin <florin.marut99@e-uvt.ro>
2024-05-13 13:16:03 +0300

    Refactor product card

--- sellsavvy/src/app/components/paged-table/paged-table.component.ts
+++ sellsavvy/src/app/components/paged-table/paged-table.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductDTO } from '../../models/dtos/product.model';
 import { UserDTO } from '../../models/dtos/user.model';
+import { Subject, takeUntil } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'paged-table',
@@ -11,9 +13,31 @@ import { UserDTO } from '../../models/dtos/user.model';
   templateUrl: './paged-table.component.html',
   styleUrl: './paged-table.component.scss',
 })
-export class PagedTableComponent {
+export class PagedTableComponent implements OnInit, OnDestroy {
   @Input() columns: number = 4;
   @Input() products: ProductDTO[] | undefined;
   @Input() loggedInUser: UserDTO | undefined;
   @Input() canEdit: boolean = false;
+
+  destroyed = new Subject<void>();
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe((result) => {
+        if (result.matches) {
+          this.columns = 1;
+        } else {
+          this.columns = 4;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
